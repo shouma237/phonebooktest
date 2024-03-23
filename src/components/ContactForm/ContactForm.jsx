3c89d8f4fc967b9a9472b2d64ce029ddd58dd75f
Doria15 <doriaceelin@gmail.com>
2024-03-23 13:41:22 +0800

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -34,7 +34,7 @@ export const ContactForm = () => {
       return;
     }
 
-    dispatch(addContact(name, number));
+    dispatch(addContact({ name, number }));
 
     setName('');
     setNumber('');
